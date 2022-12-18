769fb34f9d11070982f2368ba406708f07762e92
Ahmed Valiaiev <a.valiaiev@andersenlab.com>
2022-12-18 18:03:50 +0100

    Build geography(map) page

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,3 +1,4 @@
+import getCountryIso3 from 'country-iso-2-to-3';
 import Product from '../models/Product.js';
 import ProductStat from '../models/ProductStat.js';
 import User from '../models/User.js';
@@ -66,6 +67,31 @@ export const getTransactions = async (req, res) => {
     });
 
     res.status(200).json({ transactions, total });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+
+      if (!acc[countryISO3]) acc[countryISO3] = 0;
+
+      acc[countryISO3]++;
+
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(([country, count]) => ({
+      id: country,
+      value: count,
+    }));
+
+    res.status(200).json(formattedLocations);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 
 import clientRoutes from './routes/client.js';
-import genetalRoutes from './routes/general.js';
+import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use('/client', clientRoutes);
-app.use('/general', genetalRoutes);
+app.use('/general', generalRoutes);
 app.use('/management', managementRoutes);
 app.use('/sales', salesRoutes);
 
